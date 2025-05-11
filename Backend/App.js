fffec2270df1e43e0ce52c1c631c06410e56f5d9
Ateeb Haque <ateebhaque1912@gmail.com>
2025-05-11 12:33:54 +0530

    aa

--- Backend/App.js
+++ Backend/App.js
@@ -6,8 +6,8 @@ const db = require('./Db_Connection.js')
 const cors = require('cors');
 require('dotenv').config({ path: './.env' });
 
-app.use(express.json());
-// app.use(express.json({ limit: '20mb' }));
+// app.use(express.json());
+app.use(express.json({ limit: '20mb' }));
 
 app.use(cors({
     origin: [
@@ -32,10 +32,10 @@ app.use(session({
     }
 }));
 
-app.use(bodyParser.json());
-// app.use(bodyParser.json({ limit: '20mb' }));
-app.use(express.urlencoded({ extended: true }));
-// app.use(express.urlencoded({ extended: true, limit: '20mb' }));
+// app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '20mb' }));
+// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true, limit: '20mb' }));
 
 
 
