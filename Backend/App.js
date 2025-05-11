b9116a938a3219905681b7600879a8b868d11f05
Ateeb Haque <ateebhaque1912@gmail.com>
2025-05-11 12:26:42 +0530

    app.js

--- Backend/App.js
+++ Backend/App.js
@@ -4,13 +4,19 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const db = require('./Db_Connection.js')
 const cors = require('cors');
-require('dotenv').config({path: './.env'});
+require('dotenv').config({ path: './.env' });
 
-// app.use(express.json());
-app.use(express.json({ limit: '20mb' }));
+app.use(express.json());
+// app.use(express.json({ limit: '20mb' }));
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://217.154.86.64:3000', 'https://novibiz.com', 'https://p000tqb6-3000.inc1.devtunnels.ms' ],
+    origin: [
+        'http://localhost:3000',
+        'http://217.154.86.64:3000',
+        'https://novibiz.com',
+        'https://www.novibiz.com',
+        'https://p000tqb6-3000.inc1.devtunnels.ms'
+    ],
     methods: ["POST", "GET", 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
@@ -26,23 +32,23 @@ app.use(session({
     }
 }));
 
-// app.use(bodyParser.json());
-app.use(bodyParser.json({ limit: '20mb' }));
-// app.use(express.urlencoded({ extended: true }));
-app.use(express.urlencoded({ extended: true, limit: '20mb' }));
+app.use(bodyParser.json());
+// app.use(bodyParser.json({ limit: '20mb' }));
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true, limit: '20mb' }));
 
 
 
 db.connect((err) => {
-    if(err){
+    if (err) {
         console.log('database not connected');
-    }else{
+    } else {
         console.log('database connected');
     }
 })
 
 app.use('/hello', (req, res) => {
-    res.json({message: 'hello world'});
+    res.json({ message: 'hello world' });
 })
 
 
@@ -55,6 +61,6 @@ app.use('/notification', require('./Routers/Notification_route.js'));
 app.use('/paypal', require('./Routers/Paypal_route.js'));
 
 
-app.listen((4000), ()=> {
+app.listen((4000), () => {
     console.log('server is listing on -: http://localhost:4000')
 })
